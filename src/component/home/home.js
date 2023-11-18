6b550b80ec77fe8479af97c3a14c07ecbb563df7
Anirudha-Arjunkar <aniruddhaarjunkar@gmail.com>
2023-11-18 23:52:43 +0530

    first commit

---
+++ src/component/home/home.js
@@ -0,0 +1,11 @@
+import './home.css';
+
+function Home(){
+ return(
+    <div>
+        <h1>HELLO REACT!!!</h1>
+    </div>
+ );
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home  from './component/home/home';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <Home/>
+  </>
+);
